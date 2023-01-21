5bc8361575824bec41bea9a63f71c51b0034c57d
achchaimae <achchaimae1@gmail.com>
2023-01-21 17:59:50 +0100

    update on work

--- app/views/pages/dashboard.php
+++ app/views/pages/dashboard.php
@@ -389,11 +389,13 @@
                                                                         </td>
                                                                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
                                                                             <div class="flex space-x-4">
-                                                                                <a href="#" class="text-blue-500 hover:text-blue-600">
+                                                                                <a href="<?php echo URLROOT . '/Products/edit/'.$product->id ?>" class="text-blue-500 hover:text-blue-600">
                                                                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                                                                     </svg>
-                                                                                    <p>Edit</p>
+                                                                                    <p>
+                                                                                    edit
+                                                                                    </p>
                                                                                 </a>
                                                                                 <a href="#" class="text-red-500 hover:text-red-600">
                                                                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
