16a4014da9eb163d737b69db481b57135d8eb644
achchaimae <achchaimae1@gmail.com>
2023-01-21 20:30:22 +0100

    update dashboard 🔥

--- app/views/pages/dashboard.php
+++ app/views/pages/dashboard.php
@@ -87,7 +87,7 @@
                                             <div class="px-1 hover:text-gray-800 hover:bg-gray-100 rounded-md">
                                                 <div class="flex items-center">
                                                     <i class="fa-regular fa-circle-plus"></i>
-                                                    <a class="w-full ml-2  text-sm font-semibold text-white hover:text-gray-800  text-center" href="<?=  URLROOT;?>/inc/itemform">New item</a>
+                                                    <a class="w-full ml-2  text-sm font-semibold text-white hover:text-gray-800  text-center" href="<?= URLROOT; ?>/inc/itemform">New item</a>
                                                 </div>
                                             </div>
                                         </li>
@@ -368,47 +368,47 @@
                                                                     </tr>
                                                                 </thead>
                                                                 <!-- component -->
-                                                                <?php 
+                                                                <?php
 
                                                                 // print_r($data['products']);
-                                                                foreach($data['products'] as $product): ?>
-                                                                <tbody class="bg-white divide-y divide-gray-200">
-                                                                    <tr>
-                                                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                                                            <p><?php echo $product->name; ?></p>
-                                                                            <p class="text-xs text-gray-400">sephora
-                                                                            </p>
-                                                                        </td>
-                                                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                                                            <p><?php echo $product->quantity; ?></p>
-                                                                        </td>
-                                                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                                                            <div class="flex text-green-500">
+                                                                foreach ($data['products'] as $product) : ?>
+                                                                    <tbody class="bg-white divide-y divide-gray-200">
+                                                                        <tr>
+                                                                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                                                                <p><?php echo $product->name; ?></p>
+                                                                                <p class="text-xs text-gray-400"><?php echo $product->description; ?>
+                                                                                </p>
+                                                                            </td>
+                                                                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
                                                                                 <p><?php echo $product->quantity; ?></p>
-                                                                            </div>
-                                                                        </td>
-                                                                        <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
-                                                                            <div class="flex space-x-4">
-                                                                                <a href="<?php echo URLROOT . '/Products/edit/'.$product->id ?>" class="text-blue-500 hover:text-blue-600">
-                                                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                                                                    </svg>
-                                                                                    <p>
-                                                                                    edit
-                                                                                    </p>
-                                                                                </a>
-                                                                                <a href="#" class="text-red-500 hover:text-red-600">
-                                                                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                                                                    </svg>
-                                                                                    <p>Delete</p>
-                                                                                </a>
-                                                                            </div>
-                                                                        </td>
-                                                                    </tr>
+                                                                            </td>
+                                                                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                                                                <div class="flex text-green-500">
+                                                                                    <p><?php echo $product->price; ?></p>
+                                                                                </div>
+                                                                            </td>
+                                                                            <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
+                                                                                <div class="flex space-x-4">
+                                                                                    <a href="<?php echo URLROOT . '/Products/edit/' . $product->id ?>" class="text-blue-500 hover:text-blue-600">
+                                                                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                                                        </svg>
+                                                                                        <p>
+                                                                                            edit
+                                                                                        </p>
+                                                                                    </a>
+                                                                                    <a href="<?php echo URLROOT . '/Products/delete/'. $product->id ?>"  class="text-red-500 hover:text-red-600">
+                                                                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                                                        </svg>
+                                                                                        <p>Delete</p>
+                                                                                    </a>
+                                                                                </div>
+                                                                            </td>
+                                                                        </tr>
 
-                                                                </tbody>
-                                                            <?php endforeach;
+                                                                    </tbody>
+                                                                <?php endforeach;
                                                                 ?>
                                                             </table>
                                                         </div>
