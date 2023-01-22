4c3c054b98fe2d777d76cf7ca40c6223638c91cb
achchaimae <achchaimae1@gmail.com>
2023-01-22 16:40:38 +0100

    blog update ⛏

--- app/views/pages/blog.php
+++ app/views/pages/blog.php
@@ -46,9 +46,9 @@
             </button>
         </div>
     </section>
-    <section class="dark:bg-gray-800 dark:text-gray-100 bg-white m-4 rounded-lg">
+    <section class=" bg-white m-4 rounded-lg">
             <div class="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-                <a rel="noopener noreferrer" href="#" class="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
+                <a rel="noopener noreferrer" href="#" class="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 ">
                     <img src="<?= URLROOT; ?>/img/blog/b1.png" alt="blog" class="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500">
                     <div class="p-6 space-y-2 lg:col-span-5">
                         <h3 class="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Noster tincidunt reprimique ad pro</h3>
@@ -57,7 +57,7 @@
                     </div>
                 </a>
                 <div class="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                    <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
+                    <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline ">
                         <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500" src="<?= URLROOT; ?>/img/blog/b2.png">
                         <div class="p-6 space-y-2">
                             <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
@@ -65,7 +65,7 @@
                             <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
                         </div>
                     </a>
-                    <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
+                    <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline ">
                         <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500" src="<?= URLROOT; ?>/img/blog/b3.png">
                         <div class="p-6 space-y-2">
                             <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
@@ -73,7 +73,7 @@
                             <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
                         </div>
                     </a>
-                    <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900">
+                    <a rel="noopener noreferrer" href="#" class="max-w-sm mx-auto group hover:no-underline focus:no-underline ">
                         <img role="presentation" class="object-cover w-full rounded h-44 dark:bg-gray-500" src="<?= URLROOT; ?>/img/blog/b4.png">
                         <div class="p-6 space-y-2">
                             <h3 class="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
