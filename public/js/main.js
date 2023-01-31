6b8674273a0cca3857669e25321259cf35673a63
achchaimae <achchaimae1@gmail.com>
2023-01-31 18:41:18 +0100

    js

--- public/js/main.js
+++ public/js/main.js
@@ -9,31 +9,6 @@ const Names = document.querySelectorAll(".Names");
 //var for second function
 const items = document.querySelectorAll(".PNames");
 
-// search.addEventListener('input', listerItems);
-
-// function listerItems() {
-//     let itemshidden = 0;
-//     let filter = search.value.toLowerCase();
-//     let items = document.querySelectorAll(".Names");
-
-//     console.log(itemshidden);
-//     items.forEach(ele => {
-//         let textEle = ele.textContent;
-//         if (textEle.toLowerCase().includes(filter.toLowerCase())) {
-//             ele.style.display = '';
-//         }else{
-//             ele.style.display = "none";
-//             itemshidden++;
-//         }
-        
-//     })
-//     //for no items
-//     if (itemshidden==(items.length)){
-//         document.getElementById("noitem").style.display ='block';
-//         itemshidden==0;
-//      }else{document.getElementById("noitem").style.display ='none';}
-    
-// }
 
 
 //second method of search
