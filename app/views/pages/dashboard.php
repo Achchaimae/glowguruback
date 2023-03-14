bd3a2c8286120798b5e5f7e1d6703dd3558927f1
achchaimae <achchaimae1@gmail.com>
2023-03-14 15:30:15 +0100

    updating icon

--- app/views/pages/dashboard.php
+++ app/views/pages/dashboard.php
@@ -92,7 +92,7 @@
                                         <li class="px-2 py-1 text-white transition-colors duration-150">
                                             <div class="px-1 hover:text-gray-800 hover:bg-gray-100 rounded-md">
                                                 <div class="flex items-center">
-                                                    <i class="fa-regular fa-circle-plus"></i>
+                                                    <i class="fa-solid fa-plus"></i>
                                                     <a class="w-full ml-2  text-sm font-semibold text-white hover:text-gray-800  text-center" href="<?= URLROOT; ?>/Products/add">New item</a>
                                                 </div>
                                             </div>
